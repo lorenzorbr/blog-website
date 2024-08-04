999a98ddd7a4c3796d1de2dfaa486d26eb633e5b
Lorenzo Bragagnolo <40443452+lorenzorbr@users.noreply.github.com>
2024-08-04 16:54:49 +0000

    feat(postgres): add wait-for-postgres.js

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n🟢 Postgres está pronto e aceitando conexões\n");
+  }
+}
+
+process.stdout.write("\n\n Aguardando Postgres Aceitar Conexões ❗️");
+checkPostgres();
